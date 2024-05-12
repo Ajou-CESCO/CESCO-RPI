69db8279e36f7723ca3405b14ef356a260bfc55d
simba7743 <simba7743@ajou.ac.kr>
2024-05-12 21:43:53 +0900

    [Feat] 서버에 데이터 전송

---
+++ tests/sendRequest.py
@@ -0,0 +1,19 @@
+import requests
+import datetime
+pin_num=1
+serial_number="000000009cab1d83"
+msg=" / ".join([str(datetime.datetime.now()),str(pin_num),serial_number])
+
+data=msg.split(" / ")
+json_dict={
+    # "date":data[0],
+    "serial":data[2],
+    "index":data[1]
+}
+res=requests.post(url="http://175.45.205.29:8080/api/cabinet/sensor",
+                  json=json_dict,
+                  timeout=3
+                  )
+
+print(res.status_code)
+print(res.text)
